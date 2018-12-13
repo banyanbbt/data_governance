5110abf30fb0e5478f75793a750acecf86f38f9f
manageyp <manageyp@gmail.com>
2018-12-13 14:47:42 +0800

    [New] brand rules config file

---
+++ utils/brand_rules.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+# 汽车行业品牌名称
+automotive_brands = ['奥迪', '宝马', '奔驰', 'Volkswagen']
+
+# 保险行业品牌名称
+insurance_brands = ['中国平安', '太平', '众安', '']
+
+# 旅游行业品牌名称
+tourism_brands = ['携程', '去哪儿', 'Booking']
+
+brand_rules = {
+    'automotive': automotive_brands,
+    'insurance': insurance_brands,
+    'tourism': tourism_brands
+}
